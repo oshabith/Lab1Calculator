86437ce0d1c7db66debf93b6783d754d2106ad41
oshabith <78112024+oshabith@users.noreply.github.com>
2021-01-28 16:36:19 -0500

    "updates dataType"

--- src/ArithmaticCalculator.java
+++ src/ArithmaticCalculator.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class ArithmaticCalculator {
 
 	public static void main(String[] args) {
-		float x,y,result;
+		double x,y,result;
+		String s1,s2;
 		int choice = 0;
 		Scanner scan = new Scanner(System.in);
 		
-		while(choice != 5)
+		while(choice != 6)
 		{
 		System.out.println("--------- Arithmatic Calculator ---------");
 		System.out.println("1 for Addotion");
 		System.out.println("2 for Subtraction");
 		System.out.println("3 for Multiplication");
 		System.out.println("4 for Division");
-		System.out.println("5 for Exit");
+		System.out.println("5 for Modulus");
+		System.out.println("6 for Exit");
 		System.out.println("Enter your choice:");
 		
 		choice = scan.nextInt();
@@ -24,18 +26,22 @@ public class ArithmaticCalculator {
 		{
 		case 1:
 			System.out.println("Enter first number: ");
-			x = scan.nextFloat();
+			s1 = scan.next();
+			x =Double.parseDouble(s1);
 			System.out.println("Enter second number: ");
-			y = scan.nextFloat();
+			s2 = scan.next();
+			y = Double.parseDouble(s2);
 			result = x + y;
 			System.out.println("Result is: "+result);
 			break;
 			
 		case 2:
 			System.out.println("Enter first number: ");
-			x = scan.nextFloat();
+			s1 = scan.next();
+			x =Double.parseDouble(s1);
 			System.out.println("Enter second number: ");
-			y = scan.nextFloat();
+			s2 = scan.next();
+			y = Double.parseDouble(s2);
 			if (x>y) {
 				result = x - y;
 			}
@@ -48,18 +54,22 @@ public class ArithmaticCalculator {
 			
 		case 3:
 			System.out.println("Enter first number: ");
-			x = scan.nextFloat();
+			s1 = scan.next();
+			x =Double.parseDouble(s1);
 			System.out.println("Enter second number: ");
-			y = scan.nextFloat();
+			s2 = scan.next();
+			y = Double.parseDouble(s2);
 			result = x * y;
 			System.out.println("Result is: "+result);
 			break;
 			
 		case 4:
 			System.out.println("Enter first number: ");
-			x = scan.nextFloat();
+			s1 = scan.next();
+			x =Double.parseDouble(s1);
 			System.out.println("Enter second number: ");
-			y = scan.nextFloat();
+			s2 = scan.next();
+			y = Double.parseDouble(s2);
 			if(y == 0) {
 				result = x / y;
 				System.out.println("Cannot divide by zero");
@@ -70,7 +80,18 @@ public class ArithmaticCalculator {
 				System.out.println("Result is: "+result);
 				System.out.println();
 				break;
-			}		
+			}
+			
+		case 5:
+			System.out.println("Enter first number: ");
+			s1 = scan.next();
+			x =Double.parseDouble(s1);
+			System.out.println("Enter second number: ");
+			s2 = scan.next();
+			y = Double.parseDouble(s2);
+		    System.out.println("Result is: "+x % y);
+			break;
+				
 			
 		}//end switch
 		}//end while loop
